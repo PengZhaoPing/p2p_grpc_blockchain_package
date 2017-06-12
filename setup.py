7653b57d7f141f03ae17c29075e0872e556c7bfb
Lursun <lursun914013@gmail.com>
2017-06-12 19:24:19 +0800

    0.3

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'p2p_grpc_blockchain',
+    packages = ['p2p_grpc_blockchain','p2p_grpc_blockchain/block','p2p_grpc_blockchain/enum','p2p_grpc_blockchain/p2p','p2p_grpc_blockchain/proto','p2p_grpc_blockchain/synchronization'],
+    version = '0.3',
+    description = 'p2p_grpc_blockchain',
+    author = 'lursun & rslu',
+    author_email = 'lursun914013@gmail.com , rslu2000@gmail.com',
+    url = 'https://github.com/Lursun/p2p_grpc_blockchain/archive/v0.1.zip',
+    keywords = ['blockchain','grpc','p2p'],
+    classifiers = [],
+)
